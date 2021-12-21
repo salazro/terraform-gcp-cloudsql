a3af48dc3cd15a78eb35adbb6c523d29250ab679
charles adetiloye <charlesa101@users.noreply.github.com>
2021-12-20 19:57:54 -0600

    Update main.tf
    fixing code commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-data "google_compute_network" "vpc_network" {
+resource "google_compute_network" "vpc_network" {
   name = var.vpc_network
 }
 resource "random_string" "db_instance_suffix" {
@@ -77,13 +77,13 @@ resource "google_compute_global_address" "private-ip-peering" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 24
-  network       = data.google_compute_network.vpc_network.self_link #TODO: this is the network where the gke cluster will be paired
+  network       = "${google_compute_network.vpc_network.self_link}" #TODO: this is the network where the gke cluster will be paired
   
   depends_on = [data.google_compute_network.vpc_network]
 }
 
 resource "google_service_networking_connection" "private-vpc-connection" {
-  network = data.google_compute_network.vpc_network.self_link #TODO: this is the network where the gke cluster is deployed
+  network = "${google_compute_network.vpc_network.self_link}" #TODO: this is the network where the gke cluster is deployed
   service = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [
     google_compute_global_address.private-ip-peering.name
